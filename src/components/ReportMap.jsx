01ed0c19c16da96c39eb16956adeb44a0a3e2c70
youlim <yunsupin@gmail.com>
2025-06-21 22:03:05 +0900

    250621_Lim - missingReport 제보자 작성내용 create 받기 완료 - missingReport 에서 사용할 Map 작성 완료(위도/경도 값 create 받기 완료)

---
+++ src/components/ReportMap.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useRef, useState } from "react";
+
+const ReportMap = ({onLocationSelect}) => {
+    const [reportLocation, setReportLocation] = useState(null);
+    const markerRef = useRef(null)
+
+    useEffect(() =>{
+        const reportMapContainer = document.getElementById("reportMap");
+        const options = {
+            center: new window.kakao.maps.LatLng(37.5665, 126.9780),
+            level : 3,
+        }
+
+        const map = new window.kakao.maps.Map(reportMapContainer, options);
+        setReportLocation(map)
+
+        window.kakao.maps.event.addListener(map, "click", function(mouseEvent) {
+            const location = mouseEvent.latLng;
+
+            if(markerRef.current) {
+                markerRef.current.setMap(null)
+            }
+
+            const newMarker = new window.kakao.maps.Marker({
+                position: location
+            })
+            newMarker.setMap(map)
+            markerRef.current = newMarker
+
+            if(onLocationSelect) {
+                onLocationSelect({
+                    lat: location.getLat(),
+                    lng: location.getLng(),
+                })
+            }
+        })
+    },[])
+  return <div id="reportMap" style={{ width: "100%", height: "350px" }}></div>
+};
+export default ReportMap;
--- src/pages/MissingReport.jsx
+++ src/pages/MissingReport.jsx
@@ -3,8 +3,47 @@ import Header from '../components/Header';
 import Button from '../components/Button';
 import Map from '../components/Map';
 
+import ReportMap from '../components/ReportMap';
+import {useReportDispatch, useReportState} from "../contexts/ReportContext";
+
+import { useState } from 'react';
+import { data, useNavigate } from 'react-router-dom';
+
 const MissingReport = () => {
-  const onSubmitButtonClick = () => {};
+  const dispatch = useReportDispatch();
+  const nav = useNavigate();
+  const [form, setForm] = useState({
+    reportTitle: "",
+    reportContetnt: "",
+    lat: null,
+    lng: null,
+  });
+
+  const onCreate = () =>{
+    dispatch({
+      type: "CREATE",
+      data: {
+        ...form,
+      },
+    })
+  }
+
+  const onSubmitButtonClick = () => {
+    onCreate();
+    nav("/missingList");
+  };
+
+  const handleChange = (e) =>{
+    const {name, value} = e.target;
+    setForm((prev) => ({...prev, [name] : value}))
+  }
+  const onLocationSelect = (latlng) =>{
+    setForm((prev) => ({
+      ...prev,
+      lat: latlng.lat,
+      lng: latlng.lng,
+    }))
+  }
 
   return (
     <div className="MissingReport">
@@ -16,23 +55,23 @@ const MissingReport = () => {
         <div className="MissingReportForms">
           <div className="MissingReportForm">
             <h3>제목</h3>
-            <input type="text" />
-            {/* <input name="petTitle" value={form.petTitle} onChange={handleChange} /> */}
+            {/* <input type="text" /> */}
+            <input name="reportTitle" value={form.reportTitle} onChange={handleChange} />
           </div>
           <div className="MissingReportForm">
             <h3>내용</h3>
-            <textarea type="text" placeholder="상세한 설명을 적어주세요." />
-            {/* <textarea
-            name="petContent"
-            value={form.petContent}
+            {/* <textarea type="text" placeholder="상세한 설명을 적어주세요." /> */}
+            <textarea
+            name="reportContetnt"
+            value={form.reportContetnt}
             onChange={handleChange}
             placeholder="상세한 설명을 적어주세요."
-          /> */}
+          />
           </div>
           {/* 발견 장소 위치 Map 사용 : css 수정 필요 */}
           <div className="MissingReportForm">
             <h3>발견 장소</h3>
-            <Map />
+            <ReportMap onSelect={onLocationSelect}/>
           </div>
           <div className="MissingReport-btn">
             <Button onClick={onSubmitButtonClick} text={'신고하기'} type={'Square_lg'}></Button>
